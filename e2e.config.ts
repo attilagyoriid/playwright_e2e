4566a87a00946718f19de52ce3dcbbb17729f89d
attilagyoriid <attila.gyori.id@gmail.com>
2023-10-15 12:14:12 +0200

    e2e config

---
+++ e2e.config.ts
@@ -0,0 +1,42 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  globalTimeout: 60000,
+  retries: 1,
+  reporter: 'html',
+  fullyParallel: true,
+  testDir: 'tests/e2e',
+
+  use: {
+    navigationTimeout: 30000,
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    actionTimeout: 15000,
+    ignoreHTTPSErrors: true,
+    video: 'on',
+    screenshot: 'on',
+  },
+  projects: [
+    {
+      name: 'Chromium',
+      use: {
+        browserName: 'chromium',
+        launchOptions: {
+          args: ['--ignore-certificate-errors'],
+        },
+      },
+    },
+    {
+      name: 'Firefox',
+      use: {
+        browserName: 'firefox',
+      },
+    },
+    {
+      name: 'Webkit',
+      use: {
+        browserName: 'webkit',
+      },
+    },
+  ],
+});
